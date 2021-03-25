6af3ee00cd8d889b556062d03a6dbfd388e079d3
HarshaVardhanDodla <dodlaharshanani@gmail.com>
2021-03-25 12:51:36 +0530

    country names added in dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { FormControl, Select, MenuItem  } from '@material-ui/core';
 import './App.css';
 
+
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data)=>{
+        const countries = data.map((country) =>({
+          name: country.country,
+          value: country.countryInfo.iso2,
+        }));
+
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
-      <h1>Lets go a covid tracker!!</h1>
+      <div className="app__header">
+        <h1>covid-19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+          {
+            countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">sdf</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
